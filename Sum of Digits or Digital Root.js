90b3f0e2bf4e369238292442f4f0762a68c53378
Vladislav Kislenko <quale161@gmail.com>
2022-06-19 22:23:25 +0300

    Sum of Digits / Digital Root

---
+++ Sum of Digits or Digital Root.js
@@ -0,0 +1,23 @@
+/*
+Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+Example
+createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+The returned format must be correct in order to complete this challenge.
+Don't forget the space after the closing parentheses!
+Examples
+    16  -->  1 + 6 = 7
+   942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+*/
+function digital_root(n) {
+    let s = 0;
+    if (  n.length === 1) {
+        return n
+    }else{
+        n = n.toString().split("")
+        for (let i=0;i<n.length;i++){s+=parseInt(n[i])}
+        return n.length === 1 ? s : digital_root(s);
+    }
+}
